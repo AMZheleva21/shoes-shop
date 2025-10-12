7387488281ff202e17d71e4d30ce679d875b32a0
HP <AMZheleva21@codingburgas.bg>
2025-10-12 17:34:05 +0300

    Update the code

--- StepAhead/app.py
+++ StepAhead/app.py
@@ -1,19 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
 
-from controllers.admin_controller import admin_bp
+db = SQLAlchemy(app)
+
 from controllers.auth_controller import auth_bp
 from controllers.catalog_controller import catalog_bp
+from controllers.admin_controller import admin_bp
 from controllers.cart_controller import cart_bp
 
-app = Flask(__name__)
-app.secret_key = "supersecret"
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(catalog_bp, url_prefix="/catalog")
 app.register_blueprint(admin_bp, url_prefix="/admin")
 app.register_blueprint(cart_bp, url_prefix="/cart")
 
+from services import auth_service, catalog_service
 
-
+with app.app_context():
+    db.create_all()
+    auth_service.create_default_admin()
+    catalog_service.seed_products()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ StepAhead/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'store.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
